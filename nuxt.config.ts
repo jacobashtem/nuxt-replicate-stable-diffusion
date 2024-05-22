c2d792ad95cbffd9c3f7ec66d4d0d665c4b525bf
Jacob Ashtem <jacob.ashtem@gmail.com>
2024-05-22 14:50:35 +0200

    imgs domains

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,22 +2,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/image-edge', "@nuxtjs/tailwindcss"],
-  image: {
-    domains: ['replicate.com', 'replicate.delivery'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'replicate.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'replicate.delivery',
-      },
-    ],
-  },
-  netlify: {
-    baseURL: process.env.IMAGE_BASE_URL || 'https://generator-obrazkow-jashtem.netlify.app/',
-  },
   runtimeConfig: {
     public: {
       replicateApiToken: process.env.REPLICATE_API_TOKEN
