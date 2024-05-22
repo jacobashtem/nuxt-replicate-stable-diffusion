ceb0f6df76abbca6b72b0b426af157f41252eb1c
Jacob Ashtem <jacob.ashtem@gmail.com>
2024-05-22 15:39:32 +0200

    config fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/image-edge', '@nuxt/image', "@nuxtjs/tailwindcss"],
-  image: {
-    provider: 'static', // Użyj statycznych linków zamiast optymalizacji przez zewnętrzny serwis
-  },
   runtimeConfig: {
     public: {
       replicateApiToken: process.env.REPLICATE_API_TOKEN
